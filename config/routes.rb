ab8549fcbb397391e7c275eb050687ff22f45ae8
Jorik Cronenberg <jcronenberg@suse.de>
2022-01-17 15:01:44 +0100

    Day 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#main'
+
+  # get '/users/new', to: 'users#new'
+  resource :users
 end
